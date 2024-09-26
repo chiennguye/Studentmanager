9f2747c1eeb5a0d981cc5f0c5f41b1e802b5b02e
chienkoi <chiennguyen11042003@gmail.com>
2024-09-26 11:05:06 +0700

    myproject

---
+++ src/main/java/com/Studentmanager/Controller/StudentController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Studentmanager.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author chienkoi
+ */
+@Controller
+@RequestMapping(value = "/LandingPage")
+public class StudentController {
+    
+    @GetMapping("/index.html")
+    
+    public String index(){
+        return "LandingPage/index";
+    }
+     
+}
